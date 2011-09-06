operators: make BroadcastingOperator deals nicely with input shapein and fixes reshapein if shapein is known or if the data is a scalar or a vector

operators/linear.py
@@ -36,16 +36,17 @@ class ZeroOperator(ScalarOperator):
 @Symmetric
 class DiagonalOperator(BroadcastingOperator):
 
-    def __new__(cls, data, broadcast='disabled', **keywords):
+    def __new__(cls, data, broadcast='disabled', shapein=None, dtype=None,
+                **keywords):
+        data = np.array(data, dtype, copy=False)
+        if shapein is None and broadcast == 'disabled' and data.ndim > 0:
+            shapein = data.shape
         if np.all(data == 1):
-            return IdentityOperator(**keywords)
+            return ZeroOperator(shapein=shapein, dtype=dtype, **keywords)
         elif np.all(data == 0):
-            return ZeroOperator(**keywords)
-        return BroadcastingOperator.__new__(cls, data, broadcast, **keywords)
-
-    def __init__(self, data, broadcast='disabled', dtype=None, **keywords):
-        BroadcastingOperator.__init__(self, data, broadcast, dtype=dtype,
-                                               **keywords)
+            return IdentityOperator(shapein=shapein, dtype=dtype, **keywords)
+        return BroadcastingOperator.__new__(cls, data, broadcast=broadcast,
+            shapein=shapein, dtype=dtype, **keywords)
 
     def direct(self, input, output):
         if self.broadcast == 'fast':
@@ -77,9 +78,8 @@ class MaskOperator(DiagonalOperator):
     """
     We follow the convention of MaskedArray, where True means masked.
     """
-    def __init__(self, mask, broadcast='disabled', dtype=None, **keywords):
-        DiagonalOperator.__init__(self, mask, broadcast=broadcast,
-                                  dtype=np.bool8, **keywords)
+    def __init__(self, mask, dtype=None, **keywords):
+        DiagonalOperator.__init__(self, mask, dtype=np.bool8, **keywords)
         self.data = ~self.data
 
     conjugate_ = None

tests/test_linear.py
@@ -8,17 +8,17 @@ from operators import IdentityOperator, ZeroOperator, DiagonalOperator, MaskOper
 
 def test_masking():
 
-    mask = MaskOperator(1)
+    mask = MaskOperator(0)
     assert isinstance(mask, IdentityOperator)
-    mask = MaskOperator(1, shapein=(32,32), dtype=np.float32)
+    mask = MaskOperator(0, shapein=(32,32), dtype=np.float32)
     assert isinstance(mask, IdentityOperator)
     assert mask.shapein == (32,32)
     assert mask.dtype == np.float32
 
-    mask = MaskOperator(0)
+    mask = MaskOperator(1)
     assert isinstance(mask, ZeroOperator)
     mask = MaskOperator(1, shapein=(32,32), dtype=np.float32)
-    assert isinstance(mask, IdentityOperator)
+    assert isinstance(mask, ZeroOperator)
     assert mask.shapein == (32,32)
     assert mask.dtype == np.float32
 