cd2c869c095421c923f08f4cd47dae64a8031f83
Pierre Chanial <pierre.chanial@cea.fr>
2011-09-07 00:31:56 +0200

    rename RoundingOperator -> RoundOperator

--- operators/nonlinear.py
+++ operators/nonlinear.py
@@ -2,7 +2,8 @@ import numpy as np
 
 from .core import Square, Operator
 
-__all__ = ['ClipOperator', 'MaximumOperator', 'MinimumOperator', 'RoundingOperator']
+__all__ = ['ClipOperator', 'MaximumOperator', 'MinimumOperator',
+           'RoundOperator']
 
 
 @Square
@@ -24,7 +25,7 @@ class MinimumOperator(Operator):
 
 
 @Square
-class RoundingOperator(Operator):
+class RoundOperator(Operator):
     """Rounding operator.
     
     The rounding method may be one of the following:
