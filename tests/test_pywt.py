3ee9af3beb4b7b5003ab0d584667c7dc9d060725
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-09-07 15:07:13 +0200

    add tests for wavelets

---
+++ tests/test_pywt.py
@@ -0,0 +1,32 @@
+import nose
+import numpy as np
+from numpy.testing import *
+
+import pywt
+import operators
+import operators.pywt_operators
+
+sizes = ((64,),)
+shapes = ((8, 8), )
+wavelist = pywt.wavelist()
+levels = [2,]
+
+def check_wavelet_transpose(w, l, s):
+    W = operators.pywt_operators.Wavelet(w, level=l, shapein=s)
+    assert_array_almost_equal(W.todense(), W.T.todense().T)
+
+def test_wavelet_transpose():
+    for s in sizes:
+        for w in wavelist:
+            for l in levels:
+                yield check_wavelet_transpose, w, l, s
+
+def check_wavelet2_transpose(w, l, s):
+    W = operators.pywt_operators.Wavelet2(w, level=l, shapein=s)
+    assert_array_almost_equal(W.todense(), W.T.todense().T)
+
+def test_wavelet2_transpose():
+    for s in shapes:
+        for w in wavelist:
+            for l in levels:
+                yield check_wavelet2_transpose, w, l, s
