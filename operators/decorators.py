4bb5c90c1acac030440afdddc4ebf51264afe3e2
Pierre Chanial <pierre.chanial@cea.fr>
2011-09-08 00:15:57 +0200

    operators: Refactor validations in __init__.

--- operators/decorators.py
+++ operators/decorators.py
@@ -1,56 +1,47 @@
 def linear(cls):
-    cls.flags = cls.flags._replace(LINEAR=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'LINEAR':True})
     return cls
 
 def square(cls):
-    cls.flags = cls.flags._replace(SQUARE=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'SQUARE':True})
     return cls
 
 def real(cls):
-    cls.flags = cls.flags._replace(REAL=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'REAL':True})
     return cls
 
 def symmetric(cls):
-    cls.flags = cls.flags._replace(LINEAR=True,
-                                   SQUARE=True,
-                                   SYMMETRIC=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'LINEAR':True,
+                              'SQUARE':True,
+                              'SYMMETRIC':True})
     return cls
 
 def hermitian(cls):
-    cls.flags = cls.flags._replace(LINEAR=True,
-                                   SQUARE=True,
-                                   HERMITIAN=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'LINEAR':True,
+                              'SQUARE':True,
+                              'HERMITIAN':True})
     return cls
 
 def idempotent(cls):
-    cls.flags = cls.flags._replace(SQUARE=True,
-                                   IDEMPOTENT=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'SQUARE':True,
+                              'IDEMPOTENT':True})
     return cls
 
 def orthogonal(cls):
-    cls.flags = cls.flags._replace(LINEAR=True,
-                                   SQUARE=True,
-                                   ORTHOGONAL=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'LINEAR':True,
+                              'SQUARE':True,
+                              'ORTHOGONAL':True})
     return cls
     
 def unitary(cls):
-    cls.flags = cls.flags._replace(LINEAR=True,
-                                   SQUARE=True,
-                                   UNITARY=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'LINEAR':True,
+                              'SQUARE':True,
+                              'UNITARY':True})
     return cls
     
 def involutary(cls):
-    cls.flags = cls.flags._replace(SQUARE=True,
-                                   INVOLUTARY=True)
-    cls._validate_flags(cls)
+    cls._validate_flags(cls, {'SQUARE':True,
+                              'INVOLUTARY':True})
     return cls
     
     
