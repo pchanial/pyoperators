fb16a4eff0e43138bd2fcf9cf1bc48f26d5e4d32
Pierre Chanial <pierre.chanial@cea.fr>
2011-09-07 23:24:41 +0200

    Use nose test generators

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,14 +1,37 @@
 import nose
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_, assert_equal, assert_array_equal
 
-from operators.core import Operator, AdditionOperator, CompositionOperator, ScalarOperator, ndarraywrap
+from operators.core import (
+    Operator,
+    AdditionOperator,
+    CompositionOperator,
+    ScalarOperator,
+    ndarraywrap,
+)
 from operators.linear import I, O
 from operators.decorators import symmetric, square
 
-dtypes = [np.dtype(t) for t in (np.uint8, np.int8, np.uint16, np.int16,
-          np.uint32, np.int32, np.uint64, np.int64, np.float32, np.float64,
-          np.float128, np.complex64, np.complex128, np.complex256)]
+dtypes = [
+    np.dtype(t)
+    for t in (
+        np.uint8,
+        np.int8,
+        np.uint16,
+        np.int16,
+        np.uint32,
+        np.int32,
+        np.uint64,
+        np.int64,
+        np.float32,
+        np.float64,
+        np.float128,
+        np.complex64,
+        np.complex128,
+        np.complex256,
+    )
+]
+
 
 def assert_flags(operator, flags):
     if isinstance(flags, str):
@@ -17,28 +40,41 @@ def assert_flags(operator, flags):
         assert getattr(operator.flags, f.replace(' ', ''))
 
 
-def check_shapes(shapein):
-    o = Operator(shapein=shapein)
-    assert type(o.shapein) is tuple
-    assert all([isinstance(s, int) for s in o.shapein])
-
 def test_shapes():
-    for shapein in (3, [3], np.array(3), np.array([3]), (3,),
-                    3., [3.], np.array(3.), np.array([3.]), (3.,),
-                    [3,2], np.array([3,2]), (3,2),
-                    [3.,2], np.array([3.,2]), (3.,2),
-                   ):
-        yield check_shapes, shapein
+    for shapein in (
+        3,
+        [3],
+        np.array(3),
+        np.array([3]),
+        (3,),
+        3.0,
+        [3.0],
+        np.array(3.0),
+        np.array([3.0]),
+        (3.0,),
+        [3, 2],
+        np.array([3, 2]),
+        (3, 2),
+        [3.0, 2],
+        np.array([3.0, 2]),
+        (3.0, 2),
+    ):
+        o = Operator(shapein=shapein)
+        yield assert_equal, type(o.shapein), tuple
+        yield assert_, all([isinstance(s, int) for s in o.shapein])
+
 
 def test_dtype1():
     value = 2.5
+
     class Op(Operator):
         def __init__(self, dtype):
             Operator.__init__(self, dtype=dtype)
+
         def direct(self, input, output):
             np.multiply(input, np.array(value, self.dtype), output)
 
-    input = complex(1,1)
+    input = complex(1, 1)
     for dop in dtypes:
         for di in dtypes:
             try:
@@ -46,8 +82,8 @@ def test_dtype1():
             except TypeError:
                 i = np.array(input.real, di)
             o = Op(dop)(i)
-            assert o.dtype == (i * np.array(value, dop)).dtype
-            assert_array_equal(o, i * np.array(value, dop))
+            yield assert_equal, o.dtype, (i * np.array(value, dop)).dtype
+            yield assert_array_equal, o, i * np.array(value, dop)
 
 
 def test_dtype2():
@@ -55,141 +91,156 @@ def test_dtype2():
         def direct(self, input, output):
             np.multiply(input, input, output)
 
-    input = complex(1,1)
+    op = Op()
+    input = complex(1, 1)
     for di in dtypes:
         try:
             i = np.array(input, di)
         except TypeError:
             i = np.array(input.real, di)
-        o = Op()(i)
-        print o.dtype, o
-        assert o.dtype == (i * i).dtype
-        assert_array_equal(o, i * i)
+        o = op(i)
+        yield assert_equal, o.dtype, (i * i).dtype
+        yield assert_array_equal, o, i * i
 
 
 def test_symmetric():
-    
-    mat = np.matrix([[2,1],[1,2]])
+    mat = np.matrix([[2, 1], [1, 2]])
+
     @symmetric
     class Op(Operator):
         def __init__(self):
             Operator.__init__(self, shapein=(2,), dtype=mat.dtype)
+
         def direct(self, input, output):
             output[:] = np.dot(mat, input)
 
     op = Op()
-    assert_flags(op, 'LINEAR,SQUARE,REAL,SYMMETRIC')
-    assert op.shape == (2,2)
-    assert op.shapeout == (2,)
-    assert op.C is op.T is op.H is op
-    assert op.I is not op
-    assert_array_equal(op([1,1]), np.array(mat * [[1],[1]]).ravel())
+    yield assert_flags, op, 'LINEAR,SQUARE,REAL,SYMMETRIC'
+    yield assert_equal, op.shape, (2, 2)
+    yield assert_equal, op.shapeout, (2,)
+    yield assert_, op.C is op.T is op.H is op
+    yield assert_, op.I is not op
+    yield assert_array_equal, op([1, 1]), np.array(mat * [[1], [1]]).ravel()
 
 
-def test_scalar_reduction():
-
-    models = 1.+I, -I, (-2) * I, -(2*I), 1.-I, 1.-2*I
+def test_scalar_reduction1():
+    models = 1.0 + I, -I, (-2) * I, -(2 * I), 1.0 - I, 1.0 - 2 * I
     results = [6, -3, -6, -6, 0, -3]
     for model, result in zip(models, results):
         for i in (np.array(3), [3], (3,), np.int(3), 3):
-            assert model(i) == result
-            assert model(i).dtype == np.float64
+            o = model(i)
+            yield assert_equal, o, result
+            yield assert_equal, o.dtype, np.float64
 
+
+def test_scalar_reduction2():
+    model = -I
     iops = '__iadd__', '__isub__', '__imul__', '__iadd__', '__imul__'
-    imodels = 2*I, 2*I, 2, O, O
+    imodels = 2 * I, 2 * I, 2, O, O
     results = [3, -3, -6, -6, 0]
     for iop, imodel, result in zip(iops, imodels, results):
         model = getattr(model, iop)(imodel)
         for i in (np.array(3), [3], (3,), np.int(3), 3):
-            assert model(i) == result
-            assert model(i).dtype == np.float64
+            yield assert_equal, model(i), result
 
 
 def test_decoratein_attribute():
     class ndarray2(np.ndarray):
         pass
+
     @square
     class AddAttribute(Operator):
         def direct(self, input, output):
             pass
+
         transpose = direct
+
         def decoratein(self, input):
             input.newattr_direct = True
+
         def decorateout(self, input):
             input.newattr_transpose = True
+
     @square
     class AddAttribute2(Operator):
         def direct(self, input, output):
             pass
+
         transpose = direct
+
         def decoratein(self, input):
             input.newattr_direct = False
+
         def decorateout(self, input):
             input.newattr_transpose = False
+
     @square
     class AddAttribute3(Operator):
         def direct(self, input, output):
             pass
+
         transpose = direct
+
         def decoratein(self, input):
             input.newattr3_direct = True
+
         def decorateout(self, input):
             input.newattr3_transpose = True
 
     inputs = [np.ones(5), np.ones(5).view(ndarray2)]
     for i in inputs:
         op = AddAttribute()
-        assert op(i).newattr_direct
-        assert op.T(i).newattr_transpose
+        yield assert_, op(i).newattr_direct
+        yield assert_, op.T(i).newattr_transpose
 
         op = AddAttribute2() * AddAttribute()
-        assert not op(i).newattr_direct
-        assert op.T(i).newattr_transpose
+        yield assert_, not op(i).newattr_direct
+        yield assert_, op.T(i).newattr_transpose
 
         op = AddAttribute3() * AddAttribute()
-        assert op(i).newattr_direct
-        assert op(i).newattr3_direct
-        assert op.T(i).newattr_transpose
-        assert op.T(i).newattr3_transpose
+        yield assert_, op(i).newattr_direct
+        yield assert_, op(i).newattr3_direct
+        yield assert_, op.T(i).newattr_transpose
+        yield assert_, op.T(i).newattr3_transpose
 
     for i_ in inputs:
         op = AddAttribute()
         i = i_.copy()
-        assert op(i,i).newattr_direct
+        yield assert_, op(i, i).newattr_direct
         if type(i) is not np.ndarray:
-            assert i.newattr_direct
+            yield assert_, i.newattr_direct
         i = i_.copy()
-        assert op.T(i,i).newattr_transpose
+        yield assert_, op.T(i, i).newattr_transpose
         if type(i) is not np.ndarray:
-            assert i.newattr_transpose
+            yield assert_, i.newattr_transpose
 
         op = AddAttribute2() * AddAttribute()
         i = i_.copy()
-        assert not op(i,i).newattr_direct
+        yield assert_, not op(i, i).newattr_direct
         if type(i) is not np.ndarray:
-            assert not i.newattr_direct
+            yield assert_, not i.newattr_direct
         i = i_.copy()
-        assert op.T(i,i).newattr_transpose
+        yield assert_, op.T(i, i).newattr_transpose
         if type(i) is not np.ndarray:
-            assert i.newattr_transpose
+            yield assert_, i.newattr_transpose
 
         op = AddAttribute3() * AddAttribute()
         i = i_.copy()
-        o = op(i,i)
-        assert o.newattr_direct
-        assert o.newattr3_direct
+        o = op(i, i)
+        yield assert_, o.newattr_direct
+        yield assert_, o.newattr3_direct
         if type(i) is not np.ndarray:
-            assert i.newattr_direct
-            assert i.newattr3_direct
+            yield assert_, i.newattr_direct
+            yield assert_, i.newattr3_direct
         i = i_.copy()
-        o = op.T(i,i)
-        assert o.newattr_transpose
-        assert o.newattr3_transpose
+        o = op.T(i, i)
+        yield assert_, o.newattr_transpose
+        yield assert_, o.newattr3_transpose
         if type(i) is not np.ndarray:
-            assert i.newattr_transpose
-            assert i.newattr3_transpose
+            assert_, i.newattr_transpose
+            assert_, i.newattr3_transpose
 
-    
+
 def test_decoratein_class():
     class ndarray2(np.ndarray):
         pass
@@ -204,7 +255,9 @@ def test_decoratein_class():
     class I2(Operator):
         def direct(self, input, output):
             pass
+
         transpose = direct
+
         def decoratein(self, output):
             output.newattr = True
 
@@ -212,84 +265,93 @@ def test_decoratein_class():
     class I3(Operator):
         def direct(self, input, output):
             pass
+
         transpose = direct
+
         def decoratein(self, input):
             input.__class__ = ndarray3
+
         def decorateout(self, input):
             input.__class__ = ndarray4
 
     inputs = [np.ones(5), np.ones(5).view(ndarray2)]
-    ops = [I, I2(), I2()*I3(), I3()*I2()]
-    results = [[np.ndarray, ndarray2],
-               [ndarraywrap, ndarray2],
-               [ndarray3, ndarray3],
-               [ndarray3, ndarray3]]
-    resultsT = [[np.ndarray, ndarray2],
-                [np.ndarray, ndarray2],
-                [ndarray4, ndarray4],
-                [ndarray4, ndarray4]]
+    ops = [I, I2(), I2() * I3(), I3() * I2()]
+    results = [
+        [np.ndarray, ndarray2],
+        [ndarraywrap, ndarray2],
+        [ndarray3, ndarray3],
+        [ndarray3, ndarray3],
+    ]
+    resultsT = [
+        [np.ndarray, ndarray2],
+        [np.ndarray, ndarray2],
+        [ndarray4, ndarray4],
+        [ndarray4, ndarray4],
+    ]
 
     for op, results_, resultsT_ in zip(ops, results, resultsT):
         for i, c, cT in zip(inputs, results_, resultsT_):
             o = op(i)
-            assert o.__class__ == c
+            yield assert_equal, o.__class__, c
             o = op.T(i)
-            assert o.__class__ == cT
+            yield assert_equal, o.__class__, cT
 
     for op, results_ in zip(ops, results):
         for i, c in zip(inputs, results_):
             i = i.copy()
             if type(i) is np.ndarray:
-                op(i,i)
-                assert i.__class__ == np.ndarray
+                op(i, i)
+                yield assert_, i.__class__ == np.ndarray
             else:
-                op(i,i)
-                assert i.__class__ == c
+                op(i, i)
+                yield assert_, i.__class__ == c
 
     for op, resultsT_ in zip(ops, resultsT):
         for i, cT in zip(inputs, resultsT_):
             i = i.copy()
             if type(i) is np.ndarray:
-                op.T(i,i)
-                assert i.__class__ == np.ndarray
+                op.T(i, i)
+                yield assert_, i.__class__ == np.ndarray
             else:
-                op.T(i,i)
-                assert i.__class__ == cT
+                op.T(i, i)
+                yield assert_, i.__class__ == cT
+
 
 def test_addition():
     class Op(Operator):
         def __init__(self, v, **keywords):
             self.v = v
             Operator.__init__(self, **keywords)
+
         def direct(self, input, output):
             np.multiply(input, self.v, output)
 
     op = np.sum([Op(v) for v in [1]])
-    assert op.__class__ is Op
+    yield assert_, op.__class__ is Op
 
-    op = np.sum([Op(v) for v in [1,2]])
-    assert op.__class__ is AdditionOperator
-    assert_array_equal(op(1), 3)
-    assert op.work[0] is not None
-    assert op.work[1] is None
+    op = np.sum([Op(v) for v in [1, 2]])
+    yield assert_, op.__class__ is AdditionOperator
+    yield assert_array_equal, op(1), 3
+    yield assert_, op.work[0] is not None
+    yield assert_, op.work[1] is None
 
-    op = np.sum([Op(v) for v in [1,2,4]])
-    assert op.__class__ is AdditionOperator
+    op = np.sum([Op(v) for v in [1, 2, 4]])
+    yield assert_, op.__class__ is AdditionOperator
 
     input = np.array(1, int)
     output = np.array(0, int)
-    assert_array_equal(op(input, output), 7)
-    assert_array_equal(input, 1)
-    assert_array_equal(output, 7)
-    assert op.work[0] is not None
-    assert op.work[1] is None
+    yield assert_array_equal, op(input, output), 7
+    yield assert_array_equal, input, 1
+    yield assert_array_equal, output, 7
+    yield assert_, op.work[0] is not None
+    yield assert_, op.work[1] is None
 
     output = input
-    assert_array_equal(op(input, output), 7)
-    assert_array_equal(input, 7)
-    assert_array_equal(output, 7)
-    assert op.work[0] is not None
-    assert op.work[1] is not None
+    yield assert_array_equal, op(input, output), 7
+    yield assert_array_equal, input, 7
+    yield assert_array_equal, output, 7
+    yield assert_, op.work[0] is not None
+    yield assert_, op.work[1] is not None
 
 
 def test_composition():
@@ -297,58 +359,59 @@ def test_composition():
         def __init__(self, v, **keywords):
             self.v = v
             Operator.__init__(self, **keywords)
+
         def direct(self, input, output):
             np.multiply(input, self.v, output)
 
     op = np.product([Op(v) for v in [1]])
-    assert op.__class__ is Op
+    yield assert_, op.__class__ is Op
 
-    op = np.product([Op(v) for v in [1,2]])
-    assert op.__class__ is CompositionOperator
-    assert_array_equal(op(1), 2)
-    assert op.work[0] is None
-    assert op.work[1] is None
+    op = np.product([Op(v) for v in [1, 2]])
+    yield assert_, op.__class__ is CompositionOperator
+    yield assert_array_equal, op(1), 2
+    yield assert_, op.work[0] is None
+    yield assert_, op.work[1] is None
 
-    op = np.product([Op(v) for v in [1,2,4]])
-    assert op.__class__ is CompositionOperator
+    op = np.product([Op(v) for v in [1, 2, 4]])
+    yield assert_, op.__class__ is CompositionOperator
 
     input = np.array(1, int)
     output = np.array(0, int)
-    assert_array_equal(op(input, output), 8)
-    assert_array_equal(input, 1)
-    assert_array_equal(output, 8)
-    assert op.work[0] is None
-    assert op.work[1] is None
+    yield assert_array_equal, op(input, output), 8
+    yield assert_array_equal, input, 1
+    yield assert_array_equal, output, 8
+    yield assert_, op.work[0] is None
+    yield assert_, op.work[1] is None
 
     output = input
-    assert_array_equal(op(input, output), 8)
-    assert_array_equal(input, 8)
-    assert_array_equal(output, 8)
-    assert op.work[0] is None
-    assert op.work[1] is None
+    yield assert_array_equal, op(input, output), 8
+    yield assert_array_equal, input, 8
+    yield assert_array_equal, output, 8
+    yield assert_, op.work[0] is None
+    yield assert_, op.work[1] is None
 
 
 def test_scalar_operator():
     s = ScalarOperator(1)
-    assert s.C is s.T is s.H is s.I is s
+    yield assert_, s.C is s.T is s.H is s.I is s
 
     s = ScalarOperator(-1)
-    assert s.C is s.T is s.H is s.I is s
+    yield assert_, s.C is s.T is s.H is s.I is s
 
-    s = ScalarOperator(2.)
-    assert s.C is s.T is s.H is s
-    assert s.I is not s
+    s = ScalarOperator(2.0)
+    yield assert_, s.C is s.T is s.H is s
+    yield assert_, s.I is not s
     for o in (s.I, s.I.C, s.I.T, s.I.H, s.I.I):
-        assert isinstance(o, ScalarOperator)
+        yield assert_, isinstance(o, ScalarOperator)
 
-    s = ScalarOperator(complex(0,1))
-    assert s.T is s
-    assert s.H is s.C
-    assert s.I not in (s, s.C)
-    assert s.I.C not in (s, s.C)
-    assert isinstance(s.C, ScalarOperator)
+    s = ScalarOperator(complex(0, 1))
+    yield assert_, s.T is s
+    yield assert_, s.H is s.C
+    yield assert_, s.I not in (s, s.C)
+    yield assert_, s.I.C not in (s, s.C)
+    yield assert_, isinstance(s.C, ScalarOperator)
     for o in (s.I, s.I.C, s.I.T, s.I.H, s.I.I):
-        assert isinstance(o, ScalarOperator)
+        yield assert_, isinstance(o, ScalarOperator)
 
 
 if __name__ == "__main__":
--- tests/test_linear.py
+++ tests/test_linear.py
@@ -3,7 +3,7 @@ from __future__ import division
 import nose
 import numpy as np
 
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_, assert_array_equal
 from operators import (
     IdentityOperator,
     ZeroOperator,
@@ -17,64 +17,64 @@ from operators import (
 def test_masking():
 
     mask = MaskOperator(0)
-    assert isinstance(mask, IdentityOperator)
+    yield assert_, isinstance(mask, IdentityOperator)
     mask = MaskOperator(0, shapein=(32, 32), dtype=np.float32)
-    assert isinstance(mask, IdentityOperator)
-    assert mask.shapein == (32, 32)
-    assert mask.dtype == np.float32
+    yield assert_, isinstance(mask, IdentityOperator)
+    yield assert_, mask.shapein == (32, 32)
+    yield assert_, mask.dtype == np.float32
 
     mask = MaskOperator(1)
-    assert isinstance(mask, ZeroOperator)
+    yield assert_, isinstance(mask, ZeroOperator)
     mask = MaskOperator(1, shapein=(32, 32), dtype=np.float32)
-    assert isinstance(mask, ZeroOperator)
-    assert mask.shapein == (32, 32)
-    assert mask.dtype == np.float32
+    yield assert_, isinstance(mask, ZeroOperator)
+    yield assert_, mask.shapein == (32, 32)
+    yield assert_, mask.dtype == np.float32
 
     b = np.array([3.0, 4.0, 1.0, 0.0, 3.0, 2.0])
     c = np.array([3.0, 4.0, 0.0, 0.0, 3.0, 0.0])
     mask = MaskOperator(np.array([0, 0.0, 1.0, 1.0, 0.0, 1], dtype=np.int8))
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
     mask = DiagonalOperator(np.array([1, 1.0, 0.0, 0.0, 1.0, 0]))
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
     mask = MaskOperator(np.array([False, False, True, True, False, True]))
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
 
     b = np.array([[3.0, 4.0], [1.0, 0.0], [3.0, 2.0]])
     c = np.array([[3.0, 4.0], [0.0, 0.0], [3.0, 0.0]])
     mask = MaskOperator(np.array([[0, 0.0], [1.0, 1.0], [0.0, 1.0]], dtype='int8'))
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
     mask = DiagonalOperator(np.array([[1, 1.0], [0.0, 0.0], [1.0, 0.0]]))
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
     mask = MaskOperator(np.array([[False, False], [True, True], [False, True]]))
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
 
     b = np.array([[[3, 4.0], [1.0, 0.0]], [[3.0, 2], [-1, 9]]])
     c = np.array([[[3, 4.0], [0.0, 0.0]], [[3.0, 0], [0, 0]]])
     mask = MaskOperator(np.array([[[0, 0.0], [1.0, 1.0]], [[0.0, 1], [1, 1]]], int))
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
 
     mask = DiagonalOperator(np.array([[[1, 1], [0.0, 0]], [[1, 0], [0, 0]]]))
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
     mask = MaskOperator(
         np.array([[[False, False], [True, True]], [[False, True], [True, True]]])
     )
-    assert np.all(mask(b) == c)
+    yield assert_, np.all(mask(b) == c)
 
     c = mask(b, b)
-    assert id(b) == id(c)
+    yield assert_, id(b) == id(c)
 
 
 def test_packing():
 
     p = PackOperator([False, True, True, False, True])
-    assert p.T.__class__ == UnpackOperator
-    assert np.allclose(p([1, 2, 3, 4, 5]), [1, 4])
-    assert np.allclose(p.T([1, 4]), [1, 0, 0, 4, 0])
+    yield assert_, p.T.__class__ == UnpackOperator
+    yield assert_, np.allclose(p([1, 2, 3, 4, 5]), [1, 4])
+    yield assert_, np.allclose(p.T([1, 4]), [1, 0, 0, 4, 0])
 
     u = UnpackOperator([False, True, True, False, True])
-    assert u.T.__class__ == PackOperator
-    assert np.allclose(u([1, 4]), [1, 0, 0, 4, 0])
-    assert np.allclose(u.T([1, 2, 3, 4, 5]), [1, 4])
+    yield assert_, u.T.__class__ == PackOperator
+    yield assert_, np.allclose(u([1, 4]), [1, 0, 0, 4, 0])
+    yield assert_, np.allclose(u.T([1, 2, 3, 4, 5]), [1, 4])
 
 
 if __name__ == "__main__":
