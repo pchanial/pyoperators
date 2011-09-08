7b840891b5359d1e95954638ebc364bd9df45712
Pierre Chanial <pierre.chanial@cea.fr>
2011-09-08 09:43:47 +0200

    operators: Rename some private routines; docstring.

--- operators/decorators.py
+++ operators/decorators.py
@@ -1,47 +1,47 @@
 def linear(cls):
-    cls._validate_flags(cls, {'LINEAR':True})
+    cls._set_flags(cls, {'LINEAR':True})
     return cls
 
 def square(cls):
-    cls._validate_flags(cls, {'SQUARE':True})
+    cls._set_flags(cls, {'SQUARE':True})
     return cls
 
 def real(cls):
-    cls._validate_flags(cls, {'REAL':True})
+    cls._set_flags(cls, {'REAL':True})
     return cls
 
 def symmetric(cls):
-    cls._validate_flags(cls, {'LINEAR':True,
-                              'SQUARE':True,
-                              'SYMMETRIC':True})
+    cls._set_flags(cls, {'LINEAR':True,
+                         'SQUARE':True,
+                         'SYMMETRIC':True})
     return cls
 
 def hermitian(cls):
-    cls._validate_flags(cls, {'LINEAR':True,
-                              'SQUARE':True,
-                              'HERMITIAN':True})
+    cls._set_flags(cls, {'LINEAR':True,
+                         'SQUARE':True,
+                         'HERMITIAN':True})
     return cls
 
 def idempotent(cls):
-    cls._validate_flags(cls, {'SQUARE':True,
-                              'IDEMPOTENT':True})
+    cls._set_flags(cls, {'SQUARE':True,
+                         'IDEMPOTENT':True})
     return cls
 
 def orthogonal(cls):
-    cls._validate_flags(cls, {'LINEAR':True,
-                              'SQUARE':True,
-                              'ORTHOGONAL':True})
+    cls._set_flags(cls, {'LINEAR':True,
+                         'SQUARE':True,
+                         'ORTHOGONAL':True})
     return cls
     
 def unitary(cls):
-    cls._validate_flags(cls, {'LINEAR':True,
-                              'SQUARE':True,
-                              'UNITARY':True})
+    cls._set_flags(cls, {'LINEAR':True,
+                         'SQUARE':True,
+                         'UNITARY':True})
     return cls
     
 def involutary(cls):
-    cls._validate_flags(cls, {'SQUARE':True,
-                              'INVOLUTARY':True})
+    cls._set_flags(cls, {'SQUARE':True,
+                         'INVOLUTARY':True})
     return cls
     
     
