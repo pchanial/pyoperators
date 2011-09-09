deafbe284f9e8ec3a0be737f2412af64934d14df
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-09-09 17:18:42 +0200

    iterative docstring

--- operators/iterative/__init__.py
+++ operators/iterative/__init__.py
@@ -1,3 +1,18 @@
+"""
+Package defining algorithm working on Operators.
+Contains the following modules:
+
+criterions: define criterions to be minimized by algorithms.
+
+optimize: a wrapper for scipy.optimize "fmin" functions.
+
+algorithms: defines iterative minimization algorithms working on
+            criterions.
+
+dli: Defines the Lanczos algorithm and the DoubleLoopInference
+     algorithm.
+"""
+
 from .criterions import *
 from .optimize import *
 from .algorithms import *
