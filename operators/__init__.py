d733e7cbe92531bf17f908bfdfe63e9810186f21
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2011-09-09 16:20:29 +0200

    Docstring for the main package

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,4 +1,25 @@
+"""
+The operators contains the following packages:
+
+core : defines the Operator class
+linear : defines standard linear operators (can be seen as matrices)
+nonlinear : defines non-linear operators (such as thresholding or rounding)
+iterative : defines iterative algorithms working with operators
+
+Optionnally, if PyWavelets ( https://github.com/nigma/pywt.git ) is
+present, the pywt_operators is loaded.
+"""
+
 from .core import *
 from .linear import *
 from .nonlinear import *
 from .iterative import *
+
+# try to import pywt
+try:
+    import pywt
+except(ImportError):
+    pass
+
+if "pywt" in locals():
+    from .pywt_operators import *
