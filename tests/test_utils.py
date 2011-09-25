8a3bdf183b2e08d7fba72dcf671fbf4729773872
Pierre Chanial <pierre.chanial@cea.fr>
2011-09-25 13:56:23 +0200

    Add tests for strenum and strplural from tamasis test suite.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,8 @@
-import nose
 import numpy as np
+from nose.tools import eq_
 
 from operators import Operator
-from operators.utils import isscalar
+from operators.utils import isscalar, strenum, strplural
 
 
 def assert_is_scalar(o):
@@ -23,5 +23,31 @@ def test_is_not_scalar():
         yield assert_is_not_scalar, o
 
 
-if __name__ == "__main__":
-    nose.run(argv=['', __file__])
+def test_strenum():
+    eq_(strenum(['blue', 'red', 'yellow'], 'or'), "'blue', 'red' or 'yellow'")
+
+
+def test_strplural():
+    def func(n, prepend, s, expected):
+        eq_(strplural('cat', n, prepend=prepend, s=s), expected)
+
+    for n, prepend, s, expected in zip(
+        4 * (0, 1, 2),
+        3 * (False,) + 3 * (True,) + 3 * (False,) + 3 * (True,),
+        6 * ('',) + 6 * (':',),
+        (
+            'cat',
+            'cat',
+            'cats',
+            'no cat',
+            '1 cat',
+            '2 cats',
+            'cat',
+            'cat:',
+            'cats:',
+            'no cat',
+            '1 cat:',
+            '2 cats:',
+        ),
+    ):
+        yield func, n, prepend, s, expected
