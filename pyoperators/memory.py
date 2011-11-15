9ace2254f7b2d875ecfe5e18a31209cb7fbc61ea
Pierre Chanial <pierre.chanial@cea.fr>
2011-11-15 17:33:36 +0100

    Better docstring.

--- pyoperators/memory.py
+++ pyoperators/memory.py
@@ -55,7 +55,7 @@ def allocate_like(a, b, description):
 
 def down():
     """
-    Move stack pointer to the bottom.
+    Move stack pointer towards the bottom.
     """
     global istack
     if istack == 0:
@@ -64,7 +64,7 @@ def down():
 
 def up():
     """
-    Move stack pointer to the top.
+    Move stack pointer towards the top.
     """
     global stack, istack
     assert istack <= len(stack)
