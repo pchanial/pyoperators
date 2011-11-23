3d344d1367be8be58040ffc894def2ffe829b07b
Pierre Chanial <pierre.chanial@cea.fr>
2011-11-23 14:20:03 +0100

    Replace some yield with assertion.

--- tests/test_linear.py
+++ tests/test_linear.py
@@ -3,7 +3,6 @@ from __future__ import division
 import nose
 import numpy as np
 
-from nose.plugins.skip import SkipTest
 from numpy.testing import assert_, assert_array_equal
 from pyoperators import Operator, IdentityOperator, ZeroOperator, DiagonalOperator, MaskOperator, PackOperator, UnpackOperator
 from pyoperators.decorators import linear
@@ -11,50 +10,50 @@ from pyoperators.decorators import linear
 def test_masking():
 
     mask = MaskOperator(0)
-    yield assert_, isinstance(mask, IdentityOperator)
+    assert isinstance(mask, IdentityOperator)
     mask = MaskOperator(0, shapein=(32,32), dtype=np.float32)
-    yield assert_, isinstance(mask, IdentityOperator)
-    yield assert_, mask.shapein == (32,32)
-    yield assert_, mask.dtype == np.float32
+    assert isinstance(mask, IdentityOperator)
+    assert mask.shapein == (32,32)
+    assert mask.dtype == np.float32
 
     mask = MaskOperator(1)
-    yield assert_, isinstance(mask, ZeroOperator)
+    assert isinstance(mask, ZeroOperator)
     mask = MaskOperator(1, shapein=(32,32), dtype=np.float32)
-    yield assert_, isinstance(mask, ZeroOperator)
-    yield assert_, mask.shapein == (32,32)
-    yield assert_, mask.dtype == np.float32
+    assert isinstance(mask, ZeroOperator)
+    assert mask.shapein == (32,32)
+    assert mask.dtype == np.float32
 
     b = np.array([3., 4., 1., 0., 3., 2.])
     c = np.array([3., 4., 0., 0., 3., 0.])
     mask = MaskOperator(np.array([0, 0., 1., 1., 0., 1], dtype=np.int8))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
     mask = DiagonalOperator(np.array([1, 1., 0., 0., 1., 0]))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
     mask = MaskOperator(np.array([False, False, True, True, False, True]))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
 
     b = np.array([[3., 4.], [1., 0.], [3., 2.]])
     c = np.array([[3., 4.], [0., 0.], [3., 0.]])
     mask = MaskOperator(np.array([[0, 0.], [1., 1.], [0., 1.]], dtype='int8'))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
     mask = DiagonalOperator(np.array([[1, 1.], [0., 0.], [1., 0.]]))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
     mask = MaskOperator(np.array([[False, False], [True, True], [False, True]]))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
 
     b = np.array([[[3, 4.], [1., 0.]], [[3., 2], [-1, 9]]])
     c = np.array([[[3, 4.], [0., 0.]], [[3., 0], [0, 0]]])
     mask = MaskOperator(np.array([[[0, 0.], [1., 1.]], [[0., 1], [1, 1]]], int))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
 
     mask = DiagonalOperator(np.array([[[1, 1], [0., 0]], [[1, 0], [0, 0]]]))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
     mask = MaskOperator(np.array([[[False, False], [True, True]],
                                   [[False, True], [True, True]]]))
-    yield assert_, np.all(mask(b) == c)
+    assert np.all(mask(b) == c)
 
     c = mask(b, b)
-    yield assert_, id(b) == id(c)
+    assert id(b) == id(c)
 
 def test_masking2():
     m = MaskOperator([True, False, True])
