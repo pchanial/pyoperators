ad7a00e4e3e08f0b372f88924557e554bff30ace
Pierre Chanial <pierre.chanial@cea.fr>
2012-01-05 20:20:49 +0100

    The flags passed through the Operator __init__'s 'flag' keyword should have the same behaviour than the decorators: for example, orthogonal implies real, linear and square in both cases. In OperatorFlag, move INVOLUTARY below IDEMPOTENT. Add decorator docstrings. Add test_decorators.py test file.

--- pyoperators/core.py
+++ pyoperators/core.py
@@ -48,9 +48,9 @@ class OperatorFlags(namedtuple('OperatorFlags',
                                 'SYMMETRIC',  # o.T = o
                                 'HERMITIAN',  # o.H = o
                                 'IDEMPOTENT', # o * o = o
+                                'INVOLUTARY', # o * o = I
                                 'ORTHOGONAL', # o * o.T = I
                                 'UNITARY',    # o * o.H = I
-                                'INVOLUTARY', # o * o = I
                                 ])):
     """Informative flags about the operator."""
     def __str__(self):
@@ -722,7 +722,15 @@ class Operator(object):
             if isinstance(flags, tuple):
                 op.flags = flags
             elif isinstance(flags, dict):
+                flags = dict((k.upper(),v) for k,v in flags.iteritems())
                 op.flags = op.flags._replace(**flags)
+                if 'SYMMETRIC' in flags or 'HERMITIAN' in flags or \
+                   'ORTHOGONAL' in flags or 'UNITARY' in flags:
+                    op.flags = op.flags._replace(LINEAR=True, SQUARE=True)
+                if 'ORTHOGONAL' in flags:
+                    op.flags = op.flags._replace(REAL=True)
+                if 'IDEMPOTENT' in flags or 'INVOLUTARY' in flags:
+                    op.flags = op.flags._replace(SQUARE=True)
             else:
                 raise ValueError('Invalid input flags.')
 
--- pyoperators/decorators.py
+++ pyoperators/decorators.py
@@ -1,61 +1,103 @@
 """
-Defines decorators for Operator subclasses. Those decorators update
-the subclass flags to specify properties of the operators (linear,
-square, ...).
+Define decorators for Operator subclasses. These decorators update
+their 'flags' attribute to specify properties such as linear, square etc.
 """
 
 def flags(**keywords):
+    """
+    Decorator that sets operator's flags.
+    """
     def func(cls):
         cls._set_flags(cls, keywords)
         return cls
     return func
 
 def linear(cls):
+    """
+    Decorator for linear operators.
+    It sets the LINEAR flags.
+    """
     cls._set_flags(cls, {'LINEAR':True})
     return cls
 
 def square(cls):
+    """
+    Decorator for square operators, i.e. operators whose input and output
+    shapes are identical.
+    It sets the SQUARE flags.
+    """
     cls._set_flags(cls, {'SQUARE':True})
     return cls
 
 def real(cls):
+    """
+    Decorator for real operators, i.e. operators that are equal to
+    their conjugate.
+    It sets the REAL flags.
+    """
     cls._set_flags(cls, {'REAL':True})
     return cls
 
 def symmetric(cls):
-    cls._set_flags(cls, {'LINEAR':True,
-                         'SQUARE':True,
-                         'SYMMETRIC':True})
+    """
+    Decorator for symmetric operators, i.e. operators that are equal to their
+    transpose.
+    It sets the LINEAR, SQUARE and SYMMETRIC flags.
+    """
+    cls._set_flags(cls, {'SYMMETRIC':True})
     return cls
 
 def hermitian(cls):
-    cls._set_flags(cls, {'LINEAR':True,
-                         'SQUARE':True,
-                         'HERMITIAN':True})
+    """
+    Decorator for hermitian operators, i.e. operators that are equal to their
+    adjoint.
+    It sets the LINEAR, SQUARE and HERMITIAN flags.
+    """
+    cls._set_flags(cls, {'HERMITIAN':True})
     return cls
 
 def idempotent(cls):
+    """
+    Decorator for idempotent operators, i.e. operators whose composition
+    by themselves is equal to themselves.
+    It sets the SQUARE and IDEMPOTENT flags.
+    """
     cls._set_flags(cls, {'IDEMPOTENT':True})
     return cls
 
+def involutary(cls):
+    """
+    Decorator for involutary operators, i.e. operators whose composition
+    by themselves is equal to the identity.
+    It sets the SQUARE and INVOLUTARY flags.
+    """
+    cls._set_flags(cls, {'INVOLUTARY':True})
+    return cls
+    
 def orthogonal(cls):
-    cls._set_flags(cls, {'REAL':True,
-                         'LINEAR':True,
-                         'SQUARE':True,
-                         'ORTHOGONAL':True})
+    """
+    Decorator for orthogonal operators, i.e. real operators whose composition
+    by their transpose is equal to the identity.
+    It sets the REAL, LINEAR, SQUARE and ORTHOGONAL flags.
+    """
+    cls._set_flags(cls, {'ORTHOGONAL':True})
     return cls
     
 def unitary(cls):
-    cls._set_flags(cls, {'LINEAR':True,
-                         'SQUARE':True,
-                         'UNITARY':True})
-    return cls
-    
-def involutary(cls):
-    cls._set_flags(cls, {'SQUARE':True,
-                         'INVOLUTARY':True})
+    """
+    Decorator for orthogonal operators, i.e. operators whose composition
+    by their adjoint is equal to the identity.
+    It sets the LINEAR, SQUARE and UNITARY flags.
+    """
+    cls._set_flags(cls, {'UNITARY':True})
     return cls
     
 def inplace(cls):
+    """
+    Decorator for inplace operators, i.e operators that can handle input and
+    output pointing to the same memory location (though the input and output
+    size may be different).
+    It sets the 'inplace' attribute to True.
+    """
     cls.inplace = True
     return cls
