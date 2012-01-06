c5153c7526d3643e06319a19caa4c899e4a52ee8
Pierre Chanial <pierre.chanial@cea.fr>
2012-01-06 15:08:24 +0100

    Add more information.

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 from distutils.core import setup
+
+def version():
+    import os, re
+    f = open(os.path.join('pyoperators', '__init__.py')).read()
+    m = re.search(r"__version__ = '(.*)'", f)
+    return m.groups()[0]
+
+version = version()
+long_description = open('README.rst').read()
+keywords = 'scientific computing'
+platforms = 'MacOS X,Linux,Solaris,Unix,Windows'
+
 setup(name='pyoperators',
-      version='0.0.0',
-      description='Non-Linear Operators, Linear Operators and Iterative algorithms',
-      author='',
-      author_email='',
-      requires=['numpy', 'scipy', ],
-      packages=['pyoperators', 'pyoperators.iterative']
-      )
+      version=version,
+      description='Operators and solvers for high-performance computing.',
+      long_description=long_description,
+      url='http://pchanial.github.com/pyoperators',
+      author='Pierre Chanial & Nicolas Barbey',
+      author_email='pierre.chanial@gmail.com & nicolas.a.barbey@gmail.com',
+      maintainer='Pierre Chanial',
+      maintainer_email='pierre.chanial@gmail.com',
+      requires=['numpy (>1.6)', 'scipy (>0.9)', 'numexpr (>2.0)'],
+      packages=['pyoperators', 'pyoperators.iterative'],
+      platforms=platforms.split(','),
+      keywords=keywords.split(','),
+      license='CeCILL-B',
+      classifiers = [
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2 :: Only",
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "Operating System :: OS Independent",
+          "Topic :: Scientific/Engineering"])
