afae43ecd78d2e46f921104bf572d23541a092ce
Pierre Chanial <pierre.chanial@cea.fr>
2012-02-01 00:13:24 +0100

    Set inplace flag for ufuncs.

--- pyoperators/core.py
+++ pyoperators/core.py
@@ -992,6 +992,9 @@ class Operator(object):
             if any([self.flags.orthogonal, self.flags.unitary, self.flags.involutary]):
                 self._set_flags('orthogonal, unitary, involutary')
 
+        if isinstance(self.direct, np.ufunc):
+            self._set_flags('inplace')
+
         if self.flags.inplace_reduction and self.direct is not None:
             if isinstance(self.direct, (types.FunctionType, types.MethodType)):
                 if isinstance(self.direct, types.MethodType):
