314f5f4bd958d2977452e45b1e6f2ac77baa6ea8
Pierre Chanial <pierre.chanial@cea.fr>
2012-06-23 00:09:51 +0200

    Move DenseOperator from linear to core.

--- tests/test_linear.py
+++ tests/test_linear.py
@@ -1,36 +1,8 @@
 from __future__ import division
 
 import numpy as np
+from pyoperators import PackOperator, UnpackOperator
 
-from pyoperators import IdentityOperator, ZeroOperator, DiagonalOperator, DenseOperator, MaskOperator, PackOperator, UnpackOperator
-from pyoperators.utils.testing import assert_eq
-
-def test_denseoperator():
-
-    def func(m, d, v):
-        expected = np.dot(m, v)
-        assert_eq(d(v), expected)
-        if d.flags.square:
-            w = v.copy()
-            d(w, w)
-            assert_eq(w, expected)
-
-    m = np.array([[1,1j],[2,2]])
-    d = DenseOperator(m)
-    for v in np.array([1+0j,0]), np.array([0+0j,1]):
-        yield func, m, d, v
-        yield func, m.T, d.T, v
-        yield func, m.T.conj(), d.H, v
-
-    m = np.array([[1,2],[3,4j],[5,6]])
-    d = DenseOperator(m)
-    for v in np.array([1+0j,0]), np.array([0+0j,1]):
-        yield func, m, d, v
-    for v in np.array([1+0j,0, 0]), np.array([0j,1, 0]), np.array([0j,0,1]):
-        yield func, m.T, d.T, v
-        yield func, m.T.conj(), d.H, v
-
-        
 def test_packing():
 
     p = PackOperator([False, True, True, False, True])
