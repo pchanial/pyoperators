f98b0ddb442c5ac74c03489c81780d58b31798bf
Pierre Chanial <pierre.chanial@cea.fr>
2012-07-11 10:39:13 +0200

    Make composite operator's input flags override the default ones.

--- pyoperators/core.py
+++ pyoperators/core.py
@@ -1752,7 +1752,10 @@ class CommutativeCompositeOperator(CompositeOperator):
             'validatein':first_is_not([o.validatein for o in operands], None),
             'validateout':first_is_not([o.validateout for o in operands], None),
         }
-        attr.update(keywords)
+        for k, v in keywords.items():
+            if k is not 'flags':
+                attr[k] = v
+        attr['flags'].update(Operator.validate_flags(keywords.get('flags', {})))
         return attr
 
     @staticmethod
@@ -1912,7 +1915,10 @@ class BlockSliceOperator(CommutativeCompositeOperator):
             'dtype':cls._find_common_type([o.dtype for o in operands]),
             'flags':cls._merge_flags(operands),
         }
-        attr.update(keywords)
+        for k, v in keywords.items():
+            if k is not 'flags':
+                attr[k] = v
+        attr['flags'].update(Operator.validate_flags(keywords.get('flags', {})))
         return attr
 
     @staticmethod
@@ -2232,7 +2238,10 @@ class CompositionOperator(NonCommutativeCompositeOperator):
             'validatein':operands[-1].validatein,
             'validateout':operands[0].validateout,
         }
-        attr.update(keywords)
+        for k, v in keywords.items():
+            if k is not 'flags':
+                attr[k] = v
+        attr['flags'].update(Operator.validate_flags(keywords.get('flags', {})))
         return attr
 
     @classmethod
