76a53b49ba84c2709e14350bb637784339952f59
Pierre Chanial <pierre.chanial@cea.fr>
2012-09-20 22:46:51 +0200

    Implement new memory manager handling (non)contiguous and (non)aligned buffers.

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,12 +3,20 @@ from pyoperators import Operator, OperatorFlags, decorators
 flags = OperatorFlags._fields
     
 def test_decorators():
-    for flag in flags:
+    def func(flag):
         if flag in ('shape_input', 'shape_output', 'inplace_reduction'):
-            continue
-        o1 = Operator(flags={flag:True})
+            return
+        value = True if flag not in ('alignment_input', 'alignment_output') \
+                else 32
+        o1 = Operator(flags={flag:value})
         class O2(Operator):
             pass
-        O2 = eval('decorators.' + flag.lower() + '(O2)')
+        if flag == 'alignment_input':
+            flag = 'aligned_input'
+        elif flag == 'alignment_output':
+            flag = 'aligned_output'
+        O2 = eval('decorators.' + flag + '(O2)')
         o2 = O2()
         assert o1.flags == o2.flags
+    for flag in flags:
+        yield func, flag
