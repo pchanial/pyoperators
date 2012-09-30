9fea4c0994fb277d6969ab17abd1139ed616236f
Pierre Chanial <pierre.chanial@cea.fr>
2012-09-30 16:27:36 +0200

    Make dependencies automatically downloadable using pip. Add pyfftw to the list.

--- setup.py
+++ setup.py
@@ -42,7 +42,12 @@ setup(
     author_email='pierre.chanial@gmail.com',
     maintainer='Pierre Chanial',
     maintainer_email='pierre.chanial@gmail.com',
-    requires=['numpy (>1.6)', 'scipy (>0.9)', 'numexpr (>2.0)'],
+    install_requires=[
+        'numpy>=1.6',
+        'numexpr>2',
+        'pyfftw',
+        'scipy>=0.9',
+    ],
     packages=['pyoperators', 'pyoperators.iterative', 'pyoperators.utils'],
     platforms=platforms.split(','),
     keywords=keywords.split(','),
