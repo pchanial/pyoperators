abfdbb7af491d576526499502e67652d1fb28c41
Pierre Chanial <pierre.chanial@cea.fr>
2012-10-01 19:04:45 +0200

    Add new StopCondition class.

---
+++ pyoperators/iterative/stopconditions.py
@@ -0,0 +1,72 @@
+"""
+Module defining stop conditions for iterative algorithms.
+
+"""
+
+from __future__ import division
+
+__all__ = ['StopCondition',
+           'MaxErrorStopCondition',
+           'MaxIterationStopCondition']
+
+class StopCondition(object):
+    """
+    A class defining stop conditions for iterative algorithms. It must be
+    called with an Algorithm instance as argument. To stop the iterations,
+    the instance must raise a StopIteration instance.
+
+    """
+    def __init__(self, condition, message):
+        self.condition = condition
+        self.message = message
+    def __call__(self, s):
+        if self.condition(s):
+            raise StopIteration(self.message)
+    def __or__(self, other):
+        return OrStopCondition([self, other])
+    def __str__(self):
+        return self.message
+
+
+class OrStopCondition(StopCondition):
+    def __init__(self, stop_conditions):
+        self.operands = tuple(stop_conditions)
+    def __call__(self, s):
+        for c in self.operands:
+            c(s)
+    def __str__(self):
+        ' or '.join(str(c) for c in self.operands)
+
+
+class NoStopCondition(StopCondition):
+    def __init__(self):
+        StopCondition.__init__(self, lambda s: False, 'no stop condition')
+    def __or__(self, other):
+        return other
+
+
+class MaxErrorStopCondition(StopCondition):
+    """
+    Stop if the 'error' attribute is less than the specified maximum tolerance.
+
+    """
+    def __init__(self, maxerror, message='The maximum error is reached.'):
+        self.maxerror = maxerror
+        StopCondition.__init__(self, lambda s: s.error <= maxerror, message)
+    def __str__(self):
+        return 'maxerror={0}'.format(self.maxerror)
+
+
+class MaxIterationStopCondition(StopCondition):
+    """
+    Stop if the 'niterations' attribute is equal to the specified maximum
+    number of iterations.
+
+    """
+    def __init__(self, maxiteration, message='The maximum number of iterations '
+                 'is reached.'):
+        self.maxiteration = maxiteration
+        StopCondition.__init__(self, lambda s: s.niterations == maxiteration,
+                               message)
+    def __str__(self):
+        return 'maxiteration={0}'.format(self.maxiteration)
---
+++ tests/test_stopconditions.py
@@ -0,0 +1,41 @@
+from __future__ import division
+
+import itertools
+
+from pyoperators.iterative.stopconditions import StopCondition
+from pyoperators.utils.testing import assert_eq, assert_raises
+
+class A():
+    pass
+
+sc1 = StopCondition(lambda s:s.a > 2, 'a>2')
+sc2 = StopCondition(lambda s:s.b > 2, 'b>2')
+sc3 = StopCondition(lambda s:s.c > 2, 'c>2')
+
+def test_stop_condition():
+    values = (1,3)
+    def func(v):
+        a = A()
+        a.a = v
+        if v > 2:
+            assert_raises(StopIteration, sc1, a)
+    for v in values:
+        yield func, v
+
+def test_stop_condition_or():
+    sc = sc1 or sc2 or sc2
+    def func(v):
+        a = A()
+        a.a, a.b, a.c = v
+        if any(_ > 2 for _ in v):
+            try:
+                sc(a)
+            except StopIteration as e:
+                if a.a > 2:
+                    assert_eq(str(e), str(sc1))
+                elif a.b > 2:
+                    assert_eq(str(e), str(sc2))
+                else:
+                    assert_eq(str(e), str(sc3))
+    for v in itertools.product((1,3), repeat=3):
+        yield func, v
