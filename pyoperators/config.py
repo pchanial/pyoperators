038d9506b4cd11d44585c440955d4807773cec75
Pierre Chanial <pierre.chanial@cea.fr>
2012-10-31 07:46:17 +0100

    User site.USER_BASE instead of ~/.local

--- pyoperators/config.py
+++ pyoperators/config.py
@@ -1,8 +1,9 @@
 import os
+import site
 
 VERSION = '0.7-dev'
 
-LOCAL_PATH = os.path.join(os.path.expanduser('~'), '.local/share/pyoperators')
+LOCAL_PATH = os.path.join(site.USER_BASE, 'share', 'pyoperators')
 if not os.path.exists(LOCAL_PATH):
     os.makedirs(LOCAL_PATH)
 
