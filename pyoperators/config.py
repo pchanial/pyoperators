a4b37abdb9b41fcdb459b8a40ef01eead12e81be
Pierre Chanial <pierre.chanial@cea.fr>
2012-10-31 09:30:01 +0100

    Make use of environment variable PYOPERATORSPATH to override site.USER_BASE/share/pyoperators.

--- pyoperators/config.py
+++ pyoperators/config.py
@@ -3,8 +3,15 @@ import site
 
 VERSION = '0.7-dev'
 
-LOCAL_PATH = os.path.join(site.USER_BASE, 'share', 'pyoperators')
+LOCAL_PATH = os.getenv('PYOPERATORSPATH')
+if LOCAL_PATH is None:
+    LOCAL_PATH = os.path.join(site.USER_BASE, 'share', 'pyoperators')
 if not os.path.exists(LOCAL_PATH):
-    os.makedirs(LOCAL_PATH)
+    try:
+        os.makedirs(LOCAL_PATH)
+    except IOError:
+        print "Warning: user path '{0}' cannot be created.".format(LOCAL_PATH)
+elif not os.access(LOCAL_PATH, os.W_OK):
+    print "Warning: the user path '{0}' is not writable.".format(LOCAL_PATH)
 
-del os
+del os, site
