4356841090983793a0d53dad9d0ce480c6d82cec
Pierre Chanial <pierre.chanial@gmail.com>
2013-12-20 23:12:14 +0100

    Make CompositionOperator subclasses correctly pick the composition rules.

--- pyoperators/core.py
+++ pyoperators/core.py
@@ -2094,9 +2094,11 @@ class NonCommutativeCompositeOperator(CompositeOperator):
     def _apply_rules(self, ops):
         if len(ops) <= 1:
             return ops
-        cls = type(self)
-        i = len(ops) - 2
 
+        # Get the NonCommutativeCompositeOperator direct subclass
+        cls = type(self).__mro__[-5]
+
+        i = len(ops) - 2
         # loop over the len(ops)-1 pairs of operands
         while i >= 0:
             o1 = ops[i]
@@ -2183,8 +2185,9 @@ class CompositionOperator(NonCommutativeCompositeOperator):
                       [m.I.T for m in s.operands]))
         self.set_rule('IH', lambda s: CompositionOperator(
                       [m.I.H for m in s.operands]))
-        self.set_rule(('.', type(self)), lambda s, o: CompositionOperator(
-                      s.operands + o.operands), CompositionOperator)
+        self.set_rule(('.', CompositionOperator), lambda s, o:
+                      CompositionOperator(s.operands + o.operands),
+                      CompositionOperator)
         self.set_rule(('.', Operator), lambda s, o: CompositionOperator(
                       s.operands + [o]), CompositionOperator)
         self.set_rule((Operator, '.'), lambda o, s: CompositionOperator(
@@ -2661,7 +2664,7 @@ class GroupOperator(CompositionOperator):
             [m.I.T for m in s.operands], name=self.__name__ + '.I.T'))
         self.set_rule(IH, lambda s: GroupOperator(
             [m.I.H for m in s.operands], name=self.__name__ + '.I.H'))
-        self.del_rule(('.', type(self)), CompositionOperator)
+        self.del_rule(('.', CompositionOperator), CompositionOperator)
         self.del_rule(('.', Operator), CompositionOperator)
         self.del_rule((Operator, '.'), CompositionOperator)
 
