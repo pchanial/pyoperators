90a498edb77a8657b863103a69ac8551209e5acc
Pierre Chanial <pierre.chanial@gmail.com>
2014-01-16 23:15:48 +0100

    Version 0.10.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from numpy.distutils.command.build_ext import build_ext
 from numpy.distutils.misc_util import get_info
 from subprocess import Popen, PIPE
 
-VERSION = '0.9'
+VERSION = '0.10'
 
 
 def version_sdist():
