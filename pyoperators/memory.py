466f4c6c6eebc938fd733aaff9e8d57dd5cc8860
Pierre Chanial <pierre.chanial@gmail.com>
2014-06-24 10:42:36 +0200

    Never reuse an array if the requested buffer has zero elements.

--- pyoperators/memory.py
+++ pyoperators/memory.py
@@ -116,7 +116,9 @@ def iscompatible(array, shape, dtype, aligned=False, contiguous=False,
             return False
         return array.shape == shape and array.itemsize == dtype.itemsize
     nbytes = product(shape) * dtype.itemsize
-    return array.nbytes >= nbytes and array.nbytes / nbytes <= tolerance
+    if nbytes == 0:
+        return False
+    return array.nbytes >= nbytes and array.nbytes <= tolerance * nbytes
 
 
 class MemoryPool(object):
