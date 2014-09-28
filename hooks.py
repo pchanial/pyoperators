b46812d4f41a1aa675cb9042f24e1a3e8cfa2017
Ghislain Antony Vaillant <ghisvail@gmail.com>
2014-09-28 19:55:05 +0100

    fix #14: automatic cythonization if cython present

--- hooks.py
+++ hooks.py
@@ -35,7 +35,6 @@ F2PY_TABLE = {
     'real': {'real32': 'float', 'real64': 'double'},
     'complex': {'real32': 'complex_float', 'real64': 'complex_double'},
 }
-RECOMPILE_CYTHON = False
 REGEX_RELEASE = '^v(?P<name>[0-9.]+)$'
 REQUIRES_FORTRAN = False
 
@@ -51,6 +50,13 @@ from numpy.distutils.core import Command
 from numpy.distutils.extension import Extension
 from subprocess import call, Popen, PIPE
 from warnings import filterwarnings
+
+try:
+    from Cython.Build import cythonize
+
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
 
 try:
     root = os.path.dirname(os.path.abspath(__file__))
@@ -152,10 +158,8 @@ def get_cmdclass():
 
 def get_extension(name, sources, **keywords):
     if any(_.endswith('.pyx') for _ in sources):
-        if RECOMPILE_CYTHON:
-            from Cython.Build import cythonize
-
-            return cythonize([Extension(name, sources, **keywords)])[0]
+        if USE_CYTHON:
+            return cythonize([Extension(name, sources, **keywords)], force=True)[0]
         sources = [_[:-3] + 'c' if _.endswith('.pyx') else _ for _ in sources]
     return Extension(name, sources, **keywords)
 
