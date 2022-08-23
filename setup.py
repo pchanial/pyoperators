c628e69e1abb9b1645fa36f007d69fa49fb2b27f
Pierre Chanial <pierre.chanial@apc.in2p3.fr>
2022-08-23 14:58:13 +0200

    Convert to src layout.

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from numpy.distutils.extension import Extension
 ext_modules = [
     Extension(
         'pyoperators.utils.cythonutils',
-        sources=['pyoperators/utils/cythonutils.pyx'],
+        sources=['src/pyoperators/utils/cythonutils.pyx'],
         include_dirs=[np.get_include()],
     ),
     Extension(
         'pyoperators.utils.ufuncs',
-        sources=['pyoperators/utils/ufuncs.c.src'],
+        sources=['src/pyoperators/utils/ufuncs.c.src'],
     ),
 ]
 
